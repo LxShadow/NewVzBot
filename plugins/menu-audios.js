94c30cda28f1c022553b1870dbbf23e292a22f45
LxShadow <152157350+LxShadow@users.noreply.github.com>
2024-07-21 00:25:28 -0600

    Update menu-audios.js

--- plugins/menu-audios.js
+++ plugins/menu-audios.js
@@ -24,7 +24,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ┃
  ┃     𝗛ola, ${taguser}
  ┃
- ┃  𝗖𝗿𝗲𝗮𝗱𝗼𝗿 : 𝑺𝒉𝒂𝒅𝒐𝒘.𝑽𝒛 🦔
+ ┃  𝗖𝗿𝗲𝗮𝗱𝗼𝗿 : 𝑺𝒉𝒂𝒅𝒐𝒘𝑽𝒛 🦔
  ┃  𝗜𝗻𝘀𝘁𝗮𝗴𝗿𝗮𝗺 : https://www.instagram.com/shadow.vz/
  ┃  𝗪𝗵𝗮𝘁𝘀𝗮𝗽𝗽 : wa.me/525541081250
  ┃  𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : 𝑺𝒉𝒂𝒅𝒐𝒘𝑽𝒏𝒕𝒔
@@ -36,48 +36,48 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ╭━      🦔 𝑴𝒆𝒏𝒖 𝑨𝒖𝒅𝒊𝒐𝒔 𝑩𝒚 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 🦔 
  ┃ *- 𝙴𝚂𝙲𝚁𝙸𝙱𝙴 𝙻𝙰𝚂 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴𝚂 𝙿𝙰𝙻𝙰𝙱𝚁𝙰𝚂 𝙾 𝙵𝚁𝙰𝚂𝙴𝚂 𝚂𝙸𝙽 𝙽𝙸𝙽𝙶𝚄𝙽 𝙿𝚁𝙴𝙵𝙸𝙹𝙾 (#, /, *, .)*
  ┃
- ┃➤ ඬ⃟🔊 _Quien es tu sempai botsito 7w7_
- ┃➤ ඬ⃟🔊 _Te diagnostico con gay_
- ┃➤ ඬ⃟🔊 _No digas eso papu_
- ┃➤ ඬ⃟🔊 _A nadie le importa_
- ┃➤ ඬ⃟🔊 _Fiesta del admin_
- ┃➤ ඬ⃟🔊 _Fiesta del administrador_ 
- ┃➤ ඬ⃟🔊 _Vivan los novios_
- ┃➤ ඬ⃟🔊 _Feliz cumpleaños_
- ┃➤ ඬ⃟🔊 _Noche de paz_
- ┃➤ ඬ⃟🔊 _Buenos dias_
- ┃➤ ඬ⃟🔊 _Buenos tardes_
- ┃➤ ඬ⃟🔊 _Buenos noches_
- ┃➤ ඬ⃟🔊 _Audio hentai_
- ┃➤ ඬ⃟🔊 _Chica lgante_
- ┃➤ ඬ⃟🔊 _Feliz navidad_
- ┃➤ ඬ⃟🔊 _Vete a la vrg_
- ┃➤ ඬ⃟🔊 _Pasa pack Bot_
- ┃➤ ඬ⃟🔊 _Atencion grupo_
- ┃➤ ඬ⃟🔊 _Marica quien_
- ┃➤ ඬ⃟🔊 _Murio el grupo_
- ┃➤ ඬ⃟🔊 _Oh me vengo_
- ┃➤ ඬ⃟🔊 _tio que rico_
- ┃➤ ඬ⃟🔊 _Viernes_
- ┃➤ ඬ⃟🔊 _Baneado_
- ┃➤ ඬ⃟🔊 _Sexo_
- ┃➤ ඬ⃟🔊 _Hola_
- ┃➤ ඬ⃟🔊 _Un pato_
- ┃➤ ඬ⃟🔊 _Nyanpasu_
- ┃➤ ඬ⃟🔊 _Te amo_
- ┃➤ ඬ⃟🔊 _Yamete_
- ┃➤ ඬ⃟🔊 _Bañate_
- ┃➤ ඬ⃟🔊 _Es puto_
- ┃➤ ඬ⃟🔊 _La biblia_
- ┃➤ ඬ⃟🔊 _Onichan_
- ┃➤ ඬ⃟🔊 _Mierda de Bot_
- ┃➤ ඬ⃟🔊 _Siuuu_
- ┃➤ ඬ⃟🔊 _Epico_
- ┃➤ ඬ⃟🔊 _Shitpost_
- ┃➤ ඬ⃟🔊 _Rawr_
- ┃➤ ඬ⃟🔊 _UwU_
- ┃➤ ඬ⃟🔊 _:c_
- ┃➤ ඬ⃟🔊 _a_
+ ┃ ඬ⃟🔊 _Quien es tu sempai botsito 7w7_
+ ┃ ඬ⃟🔊 _Te diagnostico con gay_
+ ┃ ඬ⃟🔊 _No digas eso papu_
+ ┃ ඬ⃟🔊 _A nadie le importa_
+ ┃ ඬ⃟🔊 _Fiesta del admin_
+ ┃ ඬ⃟🔊 _Fiesta del administrador_ 
+ ┃ ඬ⃟🔊 _Vivan los novios_
+ ┃ ඬ⃟🔊 _Feliz cumpleaños_
+ ┃ ඬ⃟🔊 _Noche de paz_
+ ┃ ඬ⃟🔊 _Buenos dias_
+ ┃ ඬ⃟🔊 _Buenos tardes_
+ ┃ ඬ⃟🔊 _Buenos noches_
+ ┃ ඬ⃟🔊 _Audio hentai_
+ ┃ ඬ⃟🔊 _Chica lgante_
+ ┃ ඬ⃟🔊 _Feliz navidad_
+ ┃ ඬ⃟🔊 _Vete a la vrg_
+ ┃ ඬ⃟🔊 _Pasa pack Bot_
+ ┃ ඬ⃟🔊 _Atencion grupo_
+ ┃ ඬ⃟🔊 _Marica quien_
+ ┃ ඬ⃟🔊 _Murio el grupo_
+ ┃ ඬ⃟🔊 _Oh me vengo_
+ ┃ ඬ⃟🔊 _tio que rico_
+ ┃ ඬ⃟🔊 _Viernes_
+ ┃ ඬ⃟🔊 _Baneado_
+ ┃ ඬ⃟🔊 _Sexo_
+ ┃ ඬ⃟🔊 _Hola_
+ ┃ ඬ⃟🔊 _Un pato_
+ ┃ ඬ⃟🔊 _Nyanpasu_
+ ┃ ඬ⃟🔊 _Te amo_
+ ┃ ඬ⃟🔊 _Yamete_
+ ┃ ඬ⃟🔊 _Bañate_
+ ┃ ඬ⃟🔊 _Es puto_
+ ┃ ඬ⃟🔊 _La biblia_
+ ┃ ඬ⃟🔊 _Onichan_
+ ┃ ඬ⃟🔊 _Mierda de Bot_
+ ┃ ඬ⃟🔊 _Siuuu_
+ ┃ ඬ⃟🔊 _Epico_
+ ┃ ඬ⃟🔊 _Shitpost_
+ ┃ ඬ⃟🔊 _Rawr_
+ ┃ ඬ⃟🔊 _UwU_
+ ┃ ඬ⃟🔊 _:c_
+ ┃ ඬ⃟🔊 _a_
  ╰━━━━━━━━━ 𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 ━━━━`.trim();
     if (m.isGroup) {
       // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
